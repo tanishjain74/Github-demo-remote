94be9d987ad2ca49b26f20986add1f2234844a8e
tanishjain74 <98943222+tanishjain74@users.noreply.github.com>
2022-02-03 15:00:29 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+# add implementation
+def add():
+    return x+y
+# subtract implementation
+def subtract():
+    return x-y
+# multiply implementation
+def multiply():
+    return x*y
+# divide implementation
+def divide():
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+
